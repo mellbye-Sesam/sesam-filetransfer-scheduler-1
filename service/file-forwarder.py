9e8166f16393d549985547ba3e8145020bec29f7
mellbye-Sesam <hakon.mellbye@sesam.io>
2022-06-28 14:38:18 +0200

    Update file-forwarder.py
    Add more logging, change retires and disregard timeout in connetion, add hardcoded time.sleep instead.

--- service/file-forwarder.py
+++ service/file-forwarder.py
@@ -3,13 +3,14 @@ Simple service to grab data from an URL and forward it to another URL.
 """
 from requests import session
 from sesamutils import VariablesConfig, sesam_logger
+import time
 
 required_env_vars = ["INPUT_URL", "OUTPUT_URL"]
 optional_env_vars = [("INPUT_JWT", None),
                      ("OUTPUT_JWT", None),
                      ("OUTPUT_CONTENT_TYPE", "application/json; charset=utf-8"),
-                     ("RETRIES", 3),
-                     ("TIMEOUT", 1),
+                     ("RETRIES", 4),
+                     ("TIMEOUT", 5),
                      ("LOG_TIMESTAMP", True)]
 
 config = VariablesConfig(required_env_vars, optional_env_vars)
@@ -20,38 +21,45 @@ if not config.validate():
 logger = sesam_logger("file-transfer-service", timestamp=config.LOG_TIMESTAMP)
 
 
+
 def main():
+    logger.debug(f'Starting Service')
     while config.RETRIES > 0:
         try:
             input_connection = session()
             try:
                 input_connection.headers['Authorization'] = f'bearer {config.INPUT_JWT}'
-            except AttributeError:
-                pass
+            except AttributeError as exc:
+                logger.error(f'Error when conneting to input URL {config.INPUT_URL}"\nMsg: "{exc}"')
+                config.RETRIES -= 1
+                time.sleep(config.TIMEOUT)
 
             logger.debug(f'Creating stream from input URL "{config.INPUT_URL}"')
-            res = input_connection.get(config.INPUT_URL, timeout=config.TIMEOUT)
+            res = input_connection.get(config.INPUT_URL)
             res.raise_for_status()
 
             output_connection = session()
             try:
                 output_connection.headers['Authorization'] = f'bearer: {config.OUTPUT_JWT}'
-            except AttributeError:
-                pass
+            except AttributeError as exc:
+                logger.error(f'Error when conneting to output {config.OUTPUT_URL}"\nMsg: "{exc}"')
+                config.RETRIES -= 1
+                time.sleep(config.TIMEOUT)
             output_connection.headers['Content-Type'] = config.OUTPUT_CONTENT_TYPE
 
             logger.debug(f'Streaming from input URL to output URL : "{config.OUTPUT_URL}"')
             output_response = output_connection.post(
                 config.OUTPUT_URL,
                 data=res.content,
-                timeout = config.TIMEOUT
             )
-            output_response.raise_for_status()
+            status = output_response.raise_for_status()
+            config.RETRIES = 0
 
         except Exception as exc:
             logger.error(f'Error when transferring data from "{config.INPUT_URL}"->"{config.OUTPUT_URL}"\nMsg: "{exc}"')
             config.RETRIES -= 1
-        config.RETRIES = 0
+            time.sleep(config.TIMEOUT)
+
 
 if __name__ == "__main__":
     main()
